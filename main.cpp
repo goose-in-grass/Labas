2dcc5a71d10c889978da8a202c438ca9aba2536d
goose-in-grass <alshishkov8@gmail.com>
2024-03-05 01:19:20 +0200

    complete

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+
 using namespace std;
 
 
@@ -8,22 +9,40 @@ class Triangle {
     double B;
     double C;
 public:
-    Triangle(double a, double b, double c)
-    {
+    Triangle() {
+        A = 3;
+        B = 4;
+        C = 5;
+    };
+
+    Triangle(double a, double b, double c) {
         if (((a + b > c) && (a + c > b) && (b + c > a))) {
             A = a;
             B = b;
             C = c;
         } else {
             cout << "uncorrect sides";
-            a = 0;
-            b = 0;
-            c = 0;
+            A = 3;
+            B = 4;
+            C = 5;
         }
+
     }
 
-    Triangle(const Triangle& other) : Triangle(A, B, C)
-    {
+    Triangle(double a, double b) {
+        if (((a + b > a) && (a + a > b) && (b + a > a)))
+        {
+            A = C = a;
+            B = b;
+        } else {
+            cout << "uncorrected sides, will be used default sides" << endl;
+            A = C = 3;
+            B = 4;
+        }
+
+    }
+
+    Triangle(const Triangle &other) : Triangle(A, B, C) {
         std::cout << "Copy Constr" << std::endl;
 
         A = other.A;
@@ -31,48 +50,43 @@ public:
         C = other.C;
     }
 
-    ~Triangle()
-    {
+    ~Triangle() {
         std::cout << "Class was deleted" << std::endl;
 
-    }
-
-
-    double getA() {
-        return A;
     }
 
     void setA(double a) {
         if (a > 0)
             A = a;
-        else
+        else {
             cout << "error";
+            A = 3;
+        }
+
     }
 
-    double getB() {
-        return B;
-    }
 
     void setB(double b) {
         if (b > 0)
             B = b;
-        else
+        else {
             cout << "error";
+            B = 4;
+        }
     }
 
-    double getC() {
-        return C;
-    }
 
     void setC(double c) {
         if (c > 0)
             C = c;
-        else
+        else {
             cout << "error";
+            C = 5;
+        }
     }
 
-    double getP()
-    {
+
+    double getP() {
         return A + B + C;
     }
 
@@ -80,20 +94,24 @@ public:
         return std::max(A, std::max(B, C));
     }
 
-    std::string getInfo()
-    {
+    std::string getInfo() {
         std::ostringstream oss;
-        oss << "Стороны треугольника: " << A << ", " << B << ", " << C << std::endl;
-        //oss << "Периметр: " << getP() << std::endl;
+        oss << "sides of Triangle: " << A << ", " << B << ", " << C << std::endl;
+        oss << "perimeter: " << getP() << std::endl;
         return oss.str();
     }
+
+    void printInfo() {
+        std::string info = getInfo();
+        std::cout << info;
+    }
+
 };
 
 
 int main() {
-    Triangle abc(3,5, 4);
-    abc.getInfo();
-    abc.getP();
-    abc.getMaxside();
+    Triangle abc(1,1);
+    abc.printInfo();
+    std::cout << "Max side: " << abc.getMaxside() << std::endl;
     return 0;
 }
